b918c3cbd79dbe9f3b78f34195aaf6145c2066f4
pilinux <mahir.hasan@m.pilinux.me>
2026-05-22 20:02:33 +0200

    ci: release under latest tag also

--- build-image.sh
+++
@@ -1,4 +0,0 @@
-docker buildx build --platform linux/amd64,linux/arm64 -t pilinux/better-auth-hono-migrate:0.0.1 --target migrate --output type=docker .
-docker buildx build --platform linux/amd64,linux/arm64 -t pilinux/better-auth-hono:0.0.1 --target runner --output type=docker .
-# docker push pilinux/better-auth-hono-migrate:0.0.1
-# docker push pilinux/better-auth-hono:0.0.1
